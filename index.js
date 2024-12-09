8253ea1b4ee15ca54b43a29f13f17fdd1d1d4c8e
Joao Paulo <js0918990@gmail.com>
2024-12-08 22:24:58 -0300

    Atualizando

--- index.js
+++
@@ -1,8 +0,0 @@
-function comprarCreditos() {
-    alert("Redirecionando para a página de acesso via qr-code...");
-  }
-  
-  function scrollToSection(sectionId) {
-    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
-  }
-  
